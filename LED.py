c40007439f0eec3f9886f6dad9ceeb3a6d52c4be
Yungger <91400673+Yungger@users.noreply.github.com>
2022-04-19 20:55:24 +0800

    Add files via upload

--- LED.py
+++ LED.py
@@ -2,16 +2,15 @@
 """ LED.py 測試 LED 或簡易的判斷腳位輸出是否正常?
 接線: Digital or PWM -> pin 220 電阻 -> LED+ -> LED- -> GND
 """
-import time
+import utime as time
 from machine import Pin
 
 # Arduino setup()
 led = Pin(2, Pin.OUT)  # create LED object from pin2, Set Pin2 to output
-ms_start = time.ticks_ms()  # 記錄開始測量時間
 
 # Arduino loop()
-while time.ticks_diff(time.ticks_ms(), ms_start) < 5000:  # 測試 5 秒
-    led.value(1)    # Set led turn on, 同指令 led.on()
-    time.sleep(1)
+for _ in range(5):  # 測試 5 次
     led.value(0)    # Set led turn off, 同指令 led.off()
-    time.sleep(1)
+    time.sleep(0.5)
+    led.value(1)    # Set led turn on, 同指令 led.on()
+    time.sleep(0.5)
